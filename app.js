533bcc9b575255d4470c03043d7cc3f6d5788154
himanshu5479 <himanshusaini1565@gmail.com>
2025-04-11 01:02:26 +0530

    Update navbar for responsiveness

--- app.js
+++ app.js
@@ -91,13 +91,13 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-  res.locals.success=req.flash("success");
-  res.locals.error=req.flash("error");
-  res.locals.currUser=req.user;
- 
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
   next();
-})
+});
+
 /*app.get("/demouser",async(req,res)=>{
   let fakeUser=new User({
     email: "student@gmail.com",
